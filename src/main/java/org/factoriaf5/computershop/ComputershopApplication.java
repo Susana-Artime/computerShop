bcead0c71e970cf7d1001e1296baf36f2df8b783
SusanaArtime <susartime@gmail.com>
2024-11-10 22:58:29 +0100

    subo cambios en el application.properties (habilitar la gestion de transacciones) y en la app(añado la clase para subsanar el error de que no la encontraba)

--- src/main/java/org/factoriaf5/computershop/ComputershopApplication.java
+++ src/main/java/org/factoriaf5/computershop/ComputershopApplication.java
@@ -1,13 +1,25 @@
 package org.factoriaf5.computershop;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = "org.factoriaf5.computershop")
 public class ComputershopApplication {
 
+	
 	public static void main(String[] args) {
+
 		SpringApplication.run(ComputershopApplication.class, args);
 	}
 
 }
+
+
+
+
+
+
+
+
